d66ebb52edd1aabe91226c4d527eec2a4fc081b7
rageCode153 <ChrisKonstantopoulos2002@gmail.com>
2021-09-14 17:33:14 +0300

    Changes to Media-Organizer.iml

---
+++ Media-Organizer/src/main/java/service/ApiHandler.java
@@ -0,0 +1,13 @@
+package main.java.service;
+
+public class ApiHandler {
+    private String apiKey = "insert_key";
+
+    public void search() {
+
+    }
+
+    private void getProviders(int id) {
+
+    }
+}
